99f6bef01da2ce2add0bb4cda8bba9cb3ac80834
Ritovan <147165729+RitovanDasgupta@users.noreply.github.com>
2024-03-06 21:44:52 +0530

    pre-edit commit

--- app/src/main/java/com/example/loginandregisterscreen/MainActivity.kt
+++ app/src/main/java/com/example/loginandregisterscreen/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.loginandregisterscreen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import nl.joery.animatedbottombar.AnimatedBottomBar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomBar: AnimatedBottomBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomBar=findViewById(R.id.bottom_bar)
+
+        replaceFragment(HomeFragment())
+        bottomBar.setOnTabSelectListener(object : AnimatedBottomBar.OnTabSelectListener{
+            override fun onTabSelected(
+                lastIndex: Int,
+                lastTab: AnimatedBottomBar.Tab?,
+                newIndex: Int,
+                newTab: AnimatedBottomBar.Tab
+            ) {
+
+                when(newIndex){
+
+                    0->replaceFragment(HomeFragment())
+                    1->replaceFragment(OrderFragment())
+                    2->replaceFragment(ProfileFragment())
+
+                    else->{
+
+                    }
+                }
+            }
+
+        })
+
+    }
+
+    fun replaceFragment(fragment: Fragment){
+
+        supportFragmentManager.beginTransaction().replace(R.id.fragments,fragment).commit()
     }
 }
