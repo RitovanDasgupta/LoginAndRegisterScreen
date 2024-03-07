f12ba8af52be5741a30a134f0b0e607739220baa
Ritovan <147165729+RitovanDasgupta@users.noreply.github.com>
2024-03-07 11:38:23 +0530

    signup&users class

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
+//animated bottomnav
     implementation ("nl.joery.animatedbottombar:library:1.1.0")
 }
